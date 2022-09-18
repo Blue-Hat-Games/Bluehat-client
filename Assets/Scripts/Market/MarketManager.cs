7f8270cd92777604e0f1914397a07ad6266f50ec
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-09-18 22:21:46 +0900

    Feat : animal Detail, can see my animal

--- Assets/Scripts/Market/MarketManager.cs
+++ Assets/Scripts/Market/MarketManager.cs
@@ -21,16 +21,36 @@ namespace BluehatGames
         public string order = "Newest";
         public GameObject marketItemPrefab;
 
+        public Button myAnimalButton;
 
+        public GameObject myAnimalPanel;
         public Button btnNext;
         public Button btnBefore;
 
+
+        public Button myAnimalPanelCloseBtn;
+
+        public Image myAnimalSellBtn;
+
+        public GameObject myAnimalDetailPanel;
+
+        public Button bakcToMainBtn;
+
+        public Button myAnimalDetailPanelCloseBtn;
+
+
+        public Text myAnimalDetailData;
+
+        public Text AnimalDetailData;
 
         void Start()
         {
             Debug.Log("MarketManager");
             StartCoroutine(getItemCount());
             StartCoroutine(getItems());
+
+            myAnimalPanel.SetActive(false);
+            myAnimalDetailPanel.SetActive(false);
 
             btnNext.onClick.AddListener(() =>
             {
@@ -43,6 +63,29 @@ namespace BluehatGames
                 page = page - 1;
                 StartCoroutine(getItems());
             });
+
+            myAnimalButton.onClick.AddListener(() =>
+            {
+                myAnimalPanel.SetActive(true);
+                StartCoroutine(getUserAnimal());
+
+            });
+
+            myAnimalPanelCloseBtn.onClick.AddListener(() =>
+            {
+                myAnimalPanel.SetActive(false);
+            });
+
+            bakcToMainBtn.onClick.AddListener(() =>
+            {
+                SceneManager.LoadScene(SceneName._03_Main);
+            });
+
+            myAnimalDetailPanelCloseBtn.onClick.AddListener(() =>
+            {
+                myAnimalDetailPanel.SetActive(false);
+            });
+
 
         }
 
@@ -105,16 +148,70 @@ namespace BluehatGames
                     for (int i = 0; i < parse_result.items.Length; i++)
                     {
                         GameObject itemObj = GameObject.Instantiate(marketItemPrefab);
-                        itemObj.transform.SetParent(GameObject.Find("Canvas").transform);
+                        itemObj.transform.SetParent(GameObject.Find("MarketMainPanel").transform);
+                        itemObj.transform.Find("animal_id").GetComponent<Text>().text = parse_result.items[i].id.ToString();
                         itemObj.transform.Find("animal_name").GetComponent<Text>().text = parse_result.items[i].username;
                         itemObj.transform.Find("price").GetComponent<Text>().text = parse_result.items[i].price.ToString();
                         itemObj.transform.Find("view_count").GetComponent<Text>().text = parse_result.items[i].view_count.ToString();
                         itemObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(200 + 350 * i, 0);
+                        itemObj.GetComponentInChildren<Button>().onClick.AddListener(() =>
+                        {
+                            Debug.Log("Click");
+                            Debug.Log(int.Parse(itemObj.transform.Find("animal_id").GetComponent<Text>().text));
+                            StartCoroutine(getAnimalDetail(int.Parse(itemObj.transform.Find("animal_id").GetComponent<Text>().text)));
+                        });
                     }
                 }
             }
         }
+
+
+        IEnumerator getUserAnimal()
+        {
+            string host = "https://api.bluehat.games";
+            string localhost = "http://localhost:3000";
+            string url = localhost + "/animal/get-user-animal";
+            Debug.Log($"Request to Get Item -> URL: {url}");
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+            {
+                webRequest.SetRequestHeader("Authorization", "0000");
+                yield return webRequest.SendWebRequest();
+                if (webRequest.isNetworkError)
+                {
+                    Debug.Log($"Error: {webRequest.error}");
+                }
+                else
+                {
+                    string[] nameList = new string[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+                    Debug.Log($"Received: {webRequest.downloadHandler.text}");
+                    myAnimalDetailData.text = webRequest.downloadHandler.text;
+                }
+            }
+        }
+
+        IEnumerator getAnimalDetail(int id)
+        {
+            string host = "https://api.bluehat.games";
+            string localhost = "http://localhost:3000";
+            string url = localhost + "/market?id=" + id.ToString();
+            Debug.Log($"Request to Get Item -> URL: {url}");
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+            {
+                yield return webRequest.SendWebRequest();
+                if (webRequest.isNetworkError)
+                {
+                    Debug.Log($"Error: {webRequest.error}");
+                }
+                else
+                {
+                    Debug.Log($"Received: {webRequest.downloadHandler.text}");
+                    myAnimalDetailPanel.SetActive(true);
+                    AnimalDetailData.text = webRequest.downloadHandler.text;
+                }
+            }
+        }
     }
+
 
     [Serializable]
     public class ItemCard
@@ -151,4 +248,5 @@ namespace BluehatGames
     {
         public ItemCard[] items;
     }
+
 }
