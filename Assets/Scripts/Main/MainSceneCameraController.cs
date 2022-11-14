86c11c2d3101329bada927a9a1231020d2d895cf
minjujuu <dev.minjujuu@gmail.com>
2022-11-15 00:09:36 +0900

    메인씬 카메라 수직 회전 각도 제한

--- Assets/Scripts/Main/MainSceneCameraController.cs
+++ Assets/Scripts/Main/MainSceneCameraController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MainSceneCameraController : MonoBehaviour
 {
@@ -29,22 +30,40 @@ public class MainSceneCameraController : MonoBehaviour
 
     }
 
+    public float maxUp;
+    public float maxDown;
     void Update()
     {
         // 마우스가 눌러지면,
         if (Input.GetMouseButton(0))
         {
+            if(EventSystem.current.IsPointerOverGameObject())
+            {
+                return;
+            }
+            // 회전후 타겟 바라보기
+            transform.LookAt(point);
+
             // 마우스 변화량을 얻고, 그 값에 델타타임과 속도를 곱해서 회전값 구하기
-            rotationX = Input.GetAxis("Mouse X") * Time.deltaTime * speed;
-            rotationY = Input.GetAxis("Mouse Y") * Time.deltaTime * speed;
+            var horizontalAxis = Input.GetAxis("Mouse X") * Time.deltaTime * speed;
+            var verticalAxis = Input.GetAxis("Mouse Y") * Time.deltaTime * speed;
+
+            if(rotationY + verticalAxis > maxUp)
+            {
+                verticalAxis = maxUp - rotationY;
+            }
+            else if(rotationY + verticalAxis < maxDown)
+            {
+                verticalAxis = maxDown - rotationY;
+            }
 
             // 각 축으로 회전
             // Y축은 마우스를 내릴때 카메라는 올라가야 하므로 반대로 적용
-            transform.RotateAround(point, Vector3.right, -rotationY);
-            transform.RotateAround(point, Vector3.up, rotationX);
+            transform.RotateAround(point, Vector3.right, verticalAxis);
+            transform.RotateAround(point, Vector3.up, horizontalAxis);
 
-            // 회전후 타겟 바라보기
-            transform.LookAt(point);
+            rotationY += verticalAxis;
+
         }
     }
 }
